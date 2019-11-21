34075615b996d9847c99146c6356beb0210c731a
Alicia yunike riona <55614047+yunikecia123@users.noreply.github.com>
2019-11-21 22:43:49 +0700

    Add files via upload

---
+++ program_1914370113_2.c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    for(int i=1;i<=5;i++){
+        for(int j=i;j>=1;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=5;i>=0;i--){
+        for(int j=1; j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
